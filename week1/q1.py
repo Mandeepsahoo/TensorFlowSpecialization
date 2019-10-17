9424899b216a95a01ab711f7036fe897848502c4
Mandeep Sahoo <princemandeep.sahoo29@gmail.com>
2019-10-17 22:22:22 +0530

    Update q1.py

--- week1/q1.py
+++ week1/q1.py
@@ -1,1 +1,15 @@
+import tensorflow as tf
+import keras
+import numpy as np
+
+def house_model(y_new):
+    xs = np.array([0, 1, 2, 4, 6, 8, 10], dtype=float) # Your Code Here#
+    ys = np.array([0.50, 0.100, 1.50, 2.50, 3.50, 4.50, 5.50], dtype=float) # Your Code Here#
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])]) # Your Code Here#
+    model.compile(optimizer='sgd', loss='mean_squared_error')
+    model.fit(xs,ys, epochs=100)
+    return model.predict(y_new)[0]
+
+prediction = house_model([7.0])
+print(prediction)
 
