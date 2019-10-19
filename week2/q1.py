e00719de233e1a31f543b984b02ae10fd53738e0
Mandeep Sahoo <princemandeep.sahoo29@gmail.com>
2019-10-19 22:05:55 +0530

    Update q1.py

--- week2/q1.py
+++ week2/q1.py
@@ -1,1 +1,29 @@
 
+import tensorflow as tf
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if(logs.get('acc') > 0.99):
+            print("Reached 99 percent accuracy so cancelling training!")
+            self.model.stop_training = True
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+x_train = x_train/255
+x_test = x_test/255
+
+mycallback = myCallback()
+
+model = tf.keras.models.Sequential([
+                                    
+                                    tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+                                    ])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, callbacks=[mycallback])
